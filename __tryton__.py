064e3fc43c1ff77eaf03df0687d529867c304718
Shalabh Aggarwal <shalabh.aggarwal@openlabs.co.in>
2011-05-07 12:37:43 +0530

    [ADD]setup and configuration files

---
+++ __tryton__.py
@@ -0,0 +1,19 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name': 'Nereid MailChimp Imtegration',
+    'version': '1.8.0.1',
+    'author': 'Open Labs Business Solutions',
+    'email': 'info@openlabs.co.in',
+    'website': 'http://www.openlabs.co.in/',
+    'description': '''
+        Nereid Integration with MailChimp for newsletter management
+    ''',
+    'depends': [
+        'nereid',
+    ],
+    'xml': [
+    ],
+    'translation': [
+    ],
+}
